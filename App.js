2a350b9f4852289b19bc95359705accb58f3d22c
Hackytechy <1032210658@tcetmumbai.in>
2025-11-07 01:21:09 +0530

    Second commit

--- App.js
+++ App.js
@@ -12,8 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://job-portal-client-main.vercel.app",
-      "https://job-portal-client-main.vercel.app/",
+      "https://job-project-front-end-vercel.vercel.app",
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:8000",
